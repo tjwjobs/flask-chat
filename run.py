46e2a0fe9dccda955d09362273a8f2b678d757a5
tjwjobs <tjwjobs@yahoo.com>
2020-05-27 13:58:58 +0000

    added textbox functionality and refactored code

--- run.py
+++ run.py
@@ -1,34 +1,45 @@
 import os
-from flask import Flask, render_template, request, redirect
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages= []
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp" : now, "from" : username, "message": message})
 
-def get_all_messages():
-    """Get all the messages and separate using a `br`"""
-    return "<br>".join(messages)
-
-@app.route("/")
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
 
 
 
-@app.route("/<username>")
+@app.route("/chat/<username>", methods = ["GET", "POST"])
 def user(username):
-    """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    """Add and display chat messages"""
 
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
-    add_messages(username, message)
-    return redirect("/" + username)
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        # redirect prevents messages from continuously posting
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username = username, 
+    chat_messages = messages)
+
+
 
 
 app.run(host=os.getenv('IP'),  port=int(os.getenv('PORT')), debug=True)
