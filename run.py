9a9fecce8a9bdb50044192cbcee79ab97872a8f1
tjwjobs <tjwjobs@yahoo.com>
2020-05-27 14:05:46 +0000

    added procfile for heroku deployment

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, session, url_for
 
 app = Flask(__name__)
-app.secret_key = "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages= []
 
 def add_message(username, message):
@@ -42,4 +42,4 @@ def user(username):
 
 
 
-app.run(host=os.getenv('IP'),  port=int(os.getenv('PORT')), debug=True)
+app.run(host=os.getenv('IP', '0.0.0.0'),  port=int(os.getenv('PORT', '5000')), debug=False)
